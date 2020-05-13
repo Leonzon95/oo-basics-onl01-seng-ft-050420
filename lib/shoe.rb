e652ce8822c33d1d7a2a633ee3c65e9dc29bd9cd
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-13 18:51:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,14 +2,15 @@
 
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand, :condition
   
   def initialize(brand)
     @brand = brand
   end
   
   def cobble
-    
+    puts "Your shoe is as good as new!"
+    @condition = "new"
   end
 end
   
